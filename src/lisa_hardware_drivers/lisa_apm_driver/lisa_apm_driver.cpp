452330612bf540add25353b93c8fdf0265a43b0f
Anton Mitrokhin <anton.mitrokhin@phystech.edu>
2015-05-30 10:53:11 +0300

    working on SLAM launch framework

--- src/lisa_hardware_drivers/lisa_apm_driver/lisa_apm_driver.cpp
+++ src/lisa_hardware_drivers/lisa_apm_driver/lisa_apm_driver.cpp
@@ -22,139 +22,198 @@
 #define BTN_STATE  13 // Power button state
 #define DATA_SIZE  14 // Overall array size
 
-ros::Publisher  odom_pub;
-ros::Publisher  imu_pub;
-ros::Publisher  mag_pub;
 ros::Subscriber apm_sub;
-
 ros::Time current_time, last_time;
-#define MAX_DELTA_TIME 0.1
 
-double x = 0.0;
-double y = 0.0;
-double yaw = 0.0;
 
-double raw_to_meter = 100; // 1 meter per second = raw_to_meter of raw data
-double separation = 0.175; // separation of sensors in meters
+double raw_to_meter; // 1 meter per second = raw_to_meter of raw data
+double separation;   // separation of sensors in meters
+std::string odom_topic = "lisa_apm_driver/odom";
+std::string imu_topic  = "lisa_apm_driver/imu";
+std::string odom_frame = "encoder_link";
+std::string imu_frame  = "apm_link";
+
+class Odometry {
+private:
+    ros::NodeHandle nh;
+    double separation;
+    double raw_to_meter;
+    ros::Publisher odom_pub;
+    nav_msgs::Odometry odom_msg;
+
+public:
+    double x, y, yaw; // position
+    double vx, vy, w; // velocity
+
+public:
+    Odometry(ros::NodeHandle &nh_, double sep_, double to_m_) : nh(nh_) {
+        this->x = this->y = this->yaw = 0.0;
+        this->vx = this->vy = this->w = 0.0;
+        this->separation = sep_;
+        this->raw_to_meter = to_m_;
+        this->odom_pub = this->nh.advertise<nav_msgs::Odometry>(odom_topic, 10);
+    }
+
+    void spin_once(double dt, double enc0, double enc1) {
+        double lx = dt * enc0 / this->raw_to_meter;
+        double rx = dt * enc1 / this->raw_to_meter;
+
+        double dyaw = (lx - rx) / this->separation;
+        double dx = rx, dy = 0;
+        if (dyaw != 0) {
+    	    dx = sin(dyaw) * (this->separation/2 + rx/dyaw);
+    	    dy = (1 - cos(dyaw)) * (this->separation/2 + rx/dyaw);
+        }
+
+        double delta_x = dx * cos(this->yaw) - dy * sin(this->yaw);
+        double delta_y = dx * sin(this->yaw) + dy * cos(this->yaw);
+        this->x += delta_x;
+        this->y += delta_y;
+        this->yaw += dyaw;
+
+        this->vx =  delta_x / dt;
+        this->vy =  delta_y / dt;
+        this->w  =  dyaw / dt;
+    }
+
+    void send(ros::Time current_time) {
+        this->odom_msg.header.stamp = current_time;
+        this->odom_msg.header.frame_id = odom_frame;
+
+        // set the position
+        this->odom_msg.pose.pose.position.x = this->x;
+        this->odom_msg.pose.pose.position.y = this->y;
+        this->odom_msg.pose.pose.position.z = 0;
+
+        geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(this->yaw);
+        this->odom_msg.pose.pose.orientation = odom_quat;
+
+        // set the velocity
+        this->odom_msg.twist.twist.linear.x = this->vx;
+        this->odom_msg.twist.twist.linear.y = this->vy;
+        this->odom_msg.twist.twist.angular.z = this->w;
+
+        // FIXME: check the values
+        this->odom_msg.pose.covariance =  boost::assign::list_of(1e-2)  (0) (0)  (0)  (0)  (0)
+                                                                (0) (1e-2)  (0)  (0)  (0)  (0)
+                                                                (0)   (0)  (1e6) (0)  (0)  (0)
+                                                                (0)   (0)   (0) (1e6) (0)  (0)
+                                                                (0)   (0)   (0)  (0) (1e6) (0)
+                                                                (0)   (0)   (0)  (0)  (0)  (1e-1);
+
+        this->odom_msg.twist.covariance = boost::assign::list_of(1e-2)  (0) (0)  (0)  (0)  (0)
+                                                                (0) (1e-2)  (0)  (0)  (0)  (0)
+                                                                (0)   (0)  (1e6) (0)  (0)  (0)
+                                                                (0)   (0)   (0) (1e6) (0)  (0)
+                                                                (0)   (0)   (0)  (0) (1e6) (0)
+                                                                (0)   (0)   (0)  (0)  (0)  (1e-1);
+        // publish the message 
+        odom_pub.publish(this->odom_msg);
+    }
+};
+
+
+class Imu {
+private:
+    ros::NodeHandle nh;
+    ros::Publisher imu_pub;
+    sensor_msgs::Imu imu_msg;
+    double ang[3];
+    double lin[3];
+    double mag[3];
+
+public:
+    Imu(ros::NodeHandle &nh_) : nh(nh_) {
+        this->imu_pub  = this->nh.advertise<sensor_msgs::Imu>(imu_topic, 10);
+    }
+
+    void spin_once(double dt, double ax, double ay, double az,
+                                double lx, double ly, double lz,
+                                double mx, double my, double mz) {
+        this->ang[0] = ax;
+        this->ang[1] = ay;
+        this->ang[2] = az;
+
+        this->lin[0] = lx;
+        this->lin[1] = ly;
+        this->lin[2] = lz;
+
+        this->mag[0] = mx;
+        this->mag[1] = my;
+        this->mag[2] = mz;
+    }
+
+
+    void send(ros::Time current_time) {
+        this->imu_msg.header.stamp = current_time;
+        this->imu_msg.header.frame_id = imu_frame;
+
+        geometry_msgs::Quaternion imu_quat = tf::createQuaternionMsgFromYaw(this->mag[2]);
+        this->imu_msg.orientation = imu_quat;
+        this->imu_msg.angular_velocity.x = this->ang[0];
+        this->imu_msg.angular_velocity.y = this->ang[1];
+        this->imu_msg.angular_velocity.z = this->ang[2];
+
+        this->imu_msg.linear_acceleration.x = this->lin[0];
+        this->imu_msg.linear_acceleration.y = this->lin[1];
+        this->imu_msg.linear_acceleration.z = this->lin[2];
+
+
+        // FIXME: check the values
+        this->imu_msg.orientation_covariance = boost::assign::list_of (1e6)  (0) (0)
+                                                                      (0) (1e6)  (0)
+                                                                      (0)   (0)  (1e6);
+
+        this->imu_msg.angular_velocity_covariance = boost::assign::list_of (1e-1)  (0) (0)
+                                                                           (0) (1e-1)  (0)
+                                                                           (0)   (0)  (1e-1);
+
+        this->imu_msg.linear_acceleration_covariance = boost::assign::list_of (1e-3)  (0) (0)
+                                                                              (0) (1e-3)  (0)
+                                                                              (0)   (0)  (1e-3);
+        // publish the message
+        imu_pub.publish(this->imu_msg);
+    }
+};
+
+
+boost::shared_ptr<Odometry> odom_handler;
+boost::shared_ptr<Imu>      imu_handler;
+
 
 void apmCallback(const std_msgs::Float32MultiArray::ConstPtr& apm) {
     // Filling out the velocities
     current_time = ros::Time::now();
     double dt = (current_time - last_time).toSec();
-    if (fabs(dt) > MAX_DELTA_TIME) {
+    if (fabs(dt) > 0.1) {
         last_time = current_time;
         return; // This is for the first iteration when 'last_time' is undefined
     }
 
-    double lx = dt * double(apm->data[ENC_DATA_0]) / raw_to_meter;
-    double rx = dt * double(apm->data[ENC_DATA_1]) / raw_to_meter;
+    odom_handler->spin_once(dt, apm->data[ENC_DATA_0], apm->data[ENC_DATA_1]);
+    imu_handler->spin_once(dt, apm->data[IMU_ANG_VX], apm->data[IMU_ANG_VY], apm->data[IMU_ANG_VZ],
+                               apm->data[IMU_LIN_AX], apm->data[IMU_LIN_AY], apm->data[IMU_LIN_AZ],
+                               apm->data[MAG_SCAL_X], apm->data[MAG_SCAL_Y], apm->data[MAG_SCAL_Z]);
 
-    double dyaw = (lx - rx) / separation;
-    double dx = rx, dy = 0;
-    if (dyaw != 0) {
-    	dx = sin(dyaw) * (separation/2 + rx/dyaw);
-    	dy = (1 - cos(dyaw)) * (separation/2 + rx/dyaw);
-    }
-
-    double delta_x = dx * cos(yaw) - dy * sin(yaw);
-    double delta_y = dx * sin(yaw) + dy * cos(yaw);
-    x += delta_x;
-    y += delta_y;
-    yaw += dyaw;
-
-    //next, we'll publish the odometry message over ROS
-    nav_msgs::Odometry odom;
-    odom.header.stamp = current_time;
-    odom.header.frame_id = "encoder_link";
-
-    //set the position
-    odom.pose.pose.position.x = x;
-    odom.pose.pose.position.y = y;
-    odom.pose.pose.position.z = 0;
-
-    geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(yaw);
-    odom.pose.pose.orientation = odom_quat;
-
-    //set the velocity
-    odom.child_frame_id = "base_link";
-    odom.twist.twist.linear.x = delta_x / dt;
-    odom.twist.twist.linear.y = delta_y / dt;
-    odom.twist.twist.angular.z = dyaw / dt;
-
-    //FIXME: insert normal values
-    odom.pose.covariance =  boost::assign::list_of(1e-3)  (0) (0)  (0)  (0)  (0)
-                                                  (0) (1e-3)  (0)  (0)  (0)  (0)
-                                                  (0)   (0)  (1e6) (0)  (0)  (0)
-                                                  (0)   (0)   (0) (1e6) (0)  (0)
-                                                  (0)   (0)   (0)  (0) (1e6) (0)
-                                                  (0)   (0)   (0)  (0)  (0)  (1e-3);
-
-    odom.twist.covariance = boost::assign::list_of(1e-3)  (0) (0)  (0)  (0)  (0)
-                                                  (0) (1e-3)  (0)  (0)  (0)  (0)
-                                                  (0)   (0)  (1e6) (0)  (0)  (0)
-                                                  (0)   (0)   (0) (1e6) (0)  (0)
-                                                  (0)   (0)   (0)  (0) (1e6) (0)
-                                                  (0)   (0)   (0)  (0)  (0)  (1e-3);
-    //publish the message
-    odom_pub.publish(odom);
-
-
-    //next, we'll publish the imu message over ROS
-    sensor_msgs::Imu imu;
-    imu.header.stamp = current_time;
-    imu.header.frame_id = "apm_link";
-
-    geometry_msgs::Quaternion imu_quat = tf::createQuaternionMsgFromYaw(apm->data[MAG_SCAL_Z]);
-    imu.orientation = imu_quat;
-    imu.angular_velocity.x = apm->data[IMU_ANG_VX];
-    imu.angular_velocity.y = apm->data[IMU_ANG_VY];
-    imu.angular_velocity.z = apm->data[IMU_ANG_VZ];
-
-    imu.linear_acceleration.x = apm->data[IMU_LIN_AX]; 
-    imu.linear_acceleration.y = apm->data[IMU_LIN_AY];
-    imu.linear_acceleration.z = apm->data[IMU_LIN_AZ];
-
-
-    //FIXME: insert normal values
-    imu.orientation_covariance = boost::assign::list_of (1e-3)  (0) (0)
-                                                        (0) (1e-3)  (0)
-                                                        (0)   (0)  (1e6);
-
-    imu.angular_velocity_covariance = boost::assign::list_of (1e-3)  (0) (0)
-                                                             (0) (1e-3)  (0)
-                                                             (0)   (0)  (1e6);
-
-    imu.linear_acceleration_covariance = boost::assign::list_of (1e-3)  (0) (0)
-                                                                (0) (1e-3)  (0)
-                                                                (0)   (0)  (1e6);
-
-    //publish the message
-    imu_pub.publish(imu);
-
-    //next, we'll publish the magnetometer message over ROS
-    geometry_msgs::Vector3Stamped mag;
-    mag.header.stamp = current_time;
-    mag.header.frame_id = "apm_frame";
-
-    mag.vector.x = apm->data[MAG_SCAL_X];
-    mag.vector.y = apm->data[MAG_SCAL_Y];
-    mag.vector.z = apm->data[MAG_SCAL_Z];
-
-    //publish the message
-    mag_pub.publish(mag);
+    odom_handler->send(current_time);
+    imu_handler->send(current_time);
 
     last_time = current_time;
 
-    ROS_INFO("dt: %1.6f, (%3.3f, %3.3f)", dt, x, y);
 }
 
 int main(int argc, char **argv) {
     ros::init(argc, argv, "lisa_apm_driver");
     ros::NodeHandle nh;
 
-    odom_pub = nh.advertise<nav_msgs::Odometry>("/odom", 50);
-    imu_pub  = nh.advertise<sensor_msgs::Imu>("imu/data_raw", 50);
-    mag_pub  = nh.advertise<geometry_msgs::Vector3Stamped>("imu/mag", 50);
+    if (!nh.getParam("lisa_apm_driver/wheel_separation", separation)) {ROS_ERROR("Failed to get param 'lisa_apm_driver/separation'"); ros::shutdown(); };
+    if (!nh.getParam("lisa_apm_driver/raw_to_meter", raw_to_meter)) {ROS_ERROR("Failed to get param 'lisa_apm_driver/raw_to_meter'"); ros::shutdown(); };
+    if (!nh.getParam("lisa_apm_driver/odom_frame", odom_frame)) ROS_WARN("Failed to get param 'lisa_apm_driver/odom_frame'. Default: %s", odom_frame.c_str());
+    if (!nh.getParam("lisa_apm_driver/imu_frame", imu_frame)) ROS_WARN("Failed to get param 'lisa_apm_driver/apm_link'. Default: %s", imu_frame.c_str());
+
+    odom_handler = boost::shared_ptr<Odometry> (new Odometry(nh, separation, raw_to_meter));
+    imu_handler  = boost::shared_ptr<Imu> (new Imu(nh));
+
     apm_sub  = nh.subscribe("/apm/data_raw", 50, apmCallback);
 
     current_time = ros::Time::now();
--- src/lisa_move/lisa_move.cpp
+++ src/lisa_move/lisa_move.cpp
@@ -1,6 +1,5 @@
 #include <ros/ros.h>
 #include <tf/transform_listener.h>
-#include <tf/transform_broadcaster.h>
 #include <geometry_msgs/PoseWithCovarianceStamped.h>
 
 // base class for action server
@@ -9,32 +8,51 @@
 // Action description files (check the /action dir)
 #include <lisa_move/MoveAction.h>
 
-std::string input_pose_topic;
-
-
-
-
+std::string world_frame;
+std::string base_link_frame;
 
 
 class ActionServer
 {
+private:
+    tf::StampedTransform transform;
+
 public:
     ActionServer(ros::NodeHandle nh_) :
         as_move_action  (nh_, "MoveAS",   boost::bind(&ActionServer::moveActionCB,   this, _1), false)
     {
         as_move_action.start();
+
+        try {
+            this->tf_listener.waitForTransform(base_link_frame, world_frame, ros::Time(0), ros::Duration(10.0) );
+            this->tf_listener.lookupTransform(base_link_frame, world_frame, ros::Time(0), this->transform);
+        } catch (tf::TransformException ex) {
+            ROS_ERROR("%s",ex.what());
+        }
     }
 
-    ~ActionServer(void) {}
+    ~ActionServer() {}
 
-
-
-    void moveActionCB(const lisa_move::MoveGoalConstPtr  &goal) {
+    void moveActionCB(const lisa_move::MoveGoalConstPtr &goal) {
         lisa_move::MoveResult result_;
         ros::Rate r(60);
 
-
         while(true) {
+            try {
+                this->tf_listener.lookupTransform(base_link_frame, world_frame, ros::Time(0), this->transform);
+            }
+            catch (tf::TransformException &ex) {
+                ROS_ERROR("%s",ex.what());
+                r.sleep();
+                continue;
+            }
+            
+            double sx = goal->x - this->tf_listener.getOrigin().x();
+            double sy = goal->y - this->tf_listener.getOrigin().y();
+
+
+
+
             r.sleep();
             break;
         }
@@ -44,64 +62,8 @@ public:
     }
 
 private:
-    // NodeHandle instance must be created before this line. Otherwise strange error may occur.
+    tf::TransformListener tf_listener;
     actionlib::SimpleActionServer<lisa_move::MoveAction> as_move_action;
-};
-
-
-
-
-
-
-
-
-
-class TfHandle
-{
-private:
-    ros::Subscriber sub_ekf;
-
-    ros::Rate r;
-
-
-    void get_pose_ekf(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& msg_in)
-    {
-        double roll, pitch, yaw;
-        tf::Quaternion orient(msg_in->pose.pose.orientation.x, msg_in->pose.pose.orientation.y, msg_in->pose.pose.orientation.z, msg_in->pose.pose.orientation.w);
-        orient.normalize();
-        tf::Matrix3x3(orient).getRPY(roll, pitch, yaw);
-        if (isnan(msg_in->pose.pose.orientation.x) || isnan(msg_in->pose.pose.orientation.y) || isnan(msg_in->pose.pose.orientation.z) || isnan(msg_in->pose.pose.orientation.w)) {
-            ROS_WARN("%f, %f, %f, %f", (msg_in->pose.pose.orientation.x), (msg_in->pose.pose.orientation.y), (msg_in->pose.pose.orientation.z), (msg_in->pose.pose.orientation.w));
-        }
-
-        if (isnan(roll) || isnan(pitch) || isnan(yaw)) {
-            ROS_WARN("RPY failure!!! (%f, %f, %f)", roll, pitch, yaw);
-        }
-        if (isnan(roll)) {
-            roll = 0;
-        }
-        if (isnan(pitch)) {
-            pitch = 0;
-        }
-        if (isnan(yaw)) {
-            yaw = 0;
-        }
-    }
-
-    void spin()
-    {
-        ros::spinOnce();
-        this->r.sleep();
-    }
-
-public:
-    TfHandle(ros::NodeHandle nh): r(20)
-    {
-        this->sub_ekf = nh.subscribe<geometry_msgs::PoseWithCovarianceStamped> (input_pose_topic, 1, &TfHandle::get_pose_ekf, this);
-        while (ros::ok()) {
-            this->spin();
-        }
-    }
 };
 
 
@@ -111,9 +73,9 @@ int main(int argc, char** argv)
     ros::init(argc, argv, "lisa_move");
     ros::NodeHandle nh;
 
-    if (!nh.getParam("lisa_move/input_pose_topic", input_pose_topic)) input_pose_topic = "/robot_pose_ekf/odom";
+    if (!nh.getParam("lisa_move/world_frame", world_frame)) {ROS_ERROR("Failed to get param 'lisa_move/world_frame'"); ros::shutdown(); };
+    if (!nh.getParam("lisa_move/base_link_frame", base_link_frame)) {ROS_ERROR("Failed to get param 'lisa_move/base_link_frame'"); ros::shutdown(); };
 
-    TfHandle tf_handle(nh);
     ActionServer action_server(nh);
 
     ros::spin();
